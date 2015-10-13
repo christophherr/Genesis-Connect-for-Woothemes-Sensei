81fb874899f444ab9d5787a2eccb8f26d80fc9a0
christophherr <christophherr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-13 01:08:51 +0000

    Initial commit
    git-svn-id: http://plugins.svn.wordpress.org/genesis-connect-for-woothemes-sensei/trunk@1264531 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ genesis-connect-for-woothemes-sensei.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Plugin Name: Genesis Connect for Woothemes Sensei
+ * Plugin URI: http://www.christophherr.com
+ * Description: Plugin wrapper to easily integrate the Woothemes Sensei plugin with the Genesis Framework. This plugin will only work with the Genesis Framework and its child themes.
+ * Author:      Christoph Herr
+ * Author URI:	http://www.christophherr.com
+ * Version:     1.0
+ * Text Domain: genesis-connect-for-woothemes-sensei
+ * Domain Path: /languages
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package   GenesisConnectforWoothemesSensei
+ * @author    Christoph Herr
+ * @version   1.0
+ * @license   GPL-2.0+
+ *
+ * Genesis Connect for Woothemes Sensei is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Genesis Connect for Woothemes Sensei is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Genesis Connect for Woothemes Sensei. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Prevent direct access to the plugin.
+if ( ! defined( 'ABSPATH' ) ) {
+	wp_die( _e( 'Sorry, you are not allowed to access this page directly.', 'gcfws' ) );
+}
+
+/**
+ * This function runs on plugin activation. It checks to make sure the
+ * Genesis Framework is active. If not, it deactivates itself.
+ *
+ * @since 1.0
+ */
+function gcfws_activation() {
+	if ( ! function_exists( 'genesis' ) ) {
+		// Deactivate.
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+		add_action( 'admin_notices', 'gcfws_admin_notice_message' );
+	}
+}
+
+register_activation_hook( __FILE__, 'gcfws_activation' );
+
+/**
+ * This function is triggered when the WordPress theme is changed.
+ * It checks if the Genesis Framework is active. If not, it deactivates the plugin.
+ *
+ * @since 1.0
+ */
+function gcfws_plugin_deactivate() {
+	if ( ! function_exists( 'genesis' ) ) {
+		// Deactivate.
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+		add_action( 'admin_notices', 'gcfws_admin_notice_message' );
+	}
+}
+
+add_action( 'admin_init', 'gcfws_plugin_deactivate' );
+add_action( 'switch_theme', 'gcfws_plugin_deactivate' );
+
+/**
+ * Error message if you're not using the Genesis Framework.
+ *
+ * @since 1.0
+ */
+function gcfws_admin_notice_message() {
+	$error = sprintf( _e( 'Sorry, you can\'t use the Genesis Connect for Woothemes Sensei Plugin unless the <a href="%s">Geneis Framework</a> is active. The plugin has been deactivated.', 'gcfws' ), 'http://www.studiopress.com' );
+
+	echo '<div class="error"><p>' . $error . '</p></div>';
+
+	if ( isset( $_GET['activate'] ) ) {
+		unset( $_GET['activate'] );
+	}
+}
+
+// Sensei Integration.
+/**
+ * Declare theme support for Sensei
+ *
+ * @since 1.0
+ */
+function gcfws_sensei_support() {
+		add_theme_support( 'sensei' );
+}
+
+add_action( 'after_setup_theme', 'gcfws_sensei_support' );
+
+/**
+ * Remove the default Sensei wrappers
+ */
+global $woothemes_sensei;
+remove_action( 'sensei_before_main_content', array( $woothemes_sensei->frontend, 'sensei_output_content_wrapper' ), 10 );
+remove_action( 'sensei_after_main_content', array( $woothemes_sensei->frontend, 'sensei_output_content_wrapper_end' ), 10 );
+
+
+// Add custom Sensei content wrappers for Genesis.
+/**
+ * Opening the custom Sensei wrapper
+ *
+ * @since 1.0
+ */
+function gcfws_genesis_sensei_wrapper_start() {
+		echo '<div class="content-sidebar-wrap"><main class="content" role="main" itemprop="mainContentOfPage">';
+}
+
+add_action( 'sensei_before_main_content', 'gcfws_genesis_sensei_wrapper_start', 10 );
+
+/**
+ * Closing the custom Sensei wrapper
+ *
+ * @since 1.0
+ */
+function gcfws_genesis_sensei_wrapper_end() {
+				echo'</div> <!-- end main-->';
+				get_sidebar();
+				echo'</div> <!-- end .content-sidebar-wrap-->';
+}
+
+add_action( 'sensei_after_main_content', 'gcfws_genesis_sensei_wrapper_end', 10 );
---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Genesis Connect for Woothemes Sensei ===
+
+Contributors: christophherr
+Donate link: https://www.christophherr.com/donate/
+Tags: woothemes sensei, woothemes, genesis, genesis connect, studiopress
+Requires at least: 4.1
+Tested up to: 4.3
+Stable tag: 1.0
+License: GPL-2.0+
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+
+This plugin integrates the Genesis Framework from Studiopress with the Woothemes Sensei plugin.
+
+== Description ==
+
+This plugin uses code taken from https://support.woothemes.com/hc/en-us/articles/204428855-StudioPress-Genesis.
+
+You will need the Woothemes Sensei plugin (http://www.woothemes.com/products/sensei/) 
+and the Genesis Framework and/or one of its child themes (http://www.studiopress.com).
+
+In other words, if you are not trying to integrate the Woothemes Sensei plugin with the Genesis Framework 
+and/or one of its child themes, this plugin is pretty much useless for you...
+
+This plugin will only work (i.e. activate) with the Genesis Framework and its child themes.
+
+== Instructions ==
+
+Simply install and activate the plugin.
+
+There are no settings and no settings screen.
